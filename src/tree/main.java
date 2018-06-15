11cbbc39d583d9c0927c7b13b91a8d69aa12cc7b
Aisosa Efemwonkieke <aisosaefemwonkieke@gmail.com>
2018-06-15 10:20:51 -0700

    Tree.java Edited

--- src/tree/main.java
+++ src/tree/main.java
@@ -14,12 +14,50 @@ public class main {
     /**
      * @param args the command line arguments
      */
+    public  static void testTree() {
+         Tree t = new Tree();
+        for (int i = 0; i < 1000; i++) {
+            t.add((int)(Math.random() * 1000));
+        }
+        t.inOrder();
+        t.add(24);
+        System.out.println();
+        t.inOrder();
+        System.out.println();
+        t.remove(53);
+        t.inOrder();
+        System.out.println(t.size());
+    }
+    
+    public static void testHeap() {
+        int size = 10;
+        Comparable[] c = new Comparable[size];
+        for (int i = 0; i < size; i++) {
+            c[i] = (int) (Math.random() * 100);
+            System.out.print(c[i] + " ");
+        }
+        System.out.println();
+        Heap h = new Heap(c);
+        System.out.println(h.size() + " " + h.capacity());
+        h.print();
+        
+        h.add(0);
+        System.out.println(h.size() + " " + h.capacity());
+        h.print();
+        
+        h.add(400);
+        System.out.println(h.size() + " " + h.capacity());
+        h.print();
+        int length = h.size();
+        for (int i = 0; i < 10; i++)  {
+            h.add((int) (Math.random() * 1000));
+            h.print();
+            System.out.println(h.size() + " " + h.capacity());
+        } 
+        System.out.println(h.size() + " " + h.capacity());
+    }
     public static void main(String[] args) {
-        Tree t = new Tree(5);
-        t.add(10);
-        t.add(2);
-        t.add(4);
-        t.postOrder();
+       testHeap();
     }
     
 }
